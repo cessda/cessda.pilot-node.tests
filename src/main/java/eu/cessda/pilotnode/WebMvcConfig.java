5d3589bb9332c290bab32e193da6874c126b6265
Matthew Morris <matthew.morris@cessda.eu>
2026-06-30 15:24:56 +0200

    Fix not loading resources from the configured data directory

--- src/main/java/eu/cessda/pilotnode/WebMvcConfig.java
+++ src/main/java/eu/cessda/pilotnode/WebMvcConfig.java
@@ -19,6 +19,8 @@ package eu.cessda.pilotnode;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.PathResource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -49,6 +51,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // /api/data paths
         registry.addResourceHandler("/api/data/**")
-                .addResourceLocations(dataDir, "classpath:/dashboard/data/");
+                .addResourceLocations(new PathResource(dataDir), new ClassPathResource("dashboard/data/"));
     }
 }
